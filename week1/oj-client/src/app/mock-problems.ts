65b9befe26e483a866386673fbd8233cb7d3fd2b
yunxiang886 <yunxianf@usc.edu>
2017-08-09 23:14:49 -0700

    Angular2_1.0

---
+++ week1/oj-client/src/app/mock-problems.ts
@@ -0,0 +1,30 @@
+
+import {Problem} from "./models/problem.model";
+export const PROBLEMS: Problem[]= [{
+  id:1,
+  name:"two sum",
+  desc:'Given an array of integer, find two sum',
+  difficulty:"easy"
+},
+  {
+    id:2,
+    name:"three sum",
+    desc:'Given an array of integer, find three sum',
+    difficulty:"medium"
+  },
+  {
+    id:3,
+    name:"four sum",
+    desc:'Given an array of integer, find four sum',
+    difficulty:"hard"
+  },{
+    id:4,
+    name:"triangle count",
+    desc:'Given an array of integer, triangle',
+    difficulty:"hard"
+  },{
+    id:5,
+    name:"sliding window maximum",
+    desc:'Given an array of integer, geigei',
+    difficulty:"super"
+  }];
